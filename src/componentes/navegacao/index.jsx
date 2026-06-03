f17af115ca5002d17da1359299fc57237fed2f02
João Carlos <jo029149@gmail.com>
2026-06-02 23:30:15 -0300

    feat: Barra de navegação
    Adicionei a barra de navegação, onde o temos as opções: início, perfil e sair da conta.

--- src/componentes/navegacao/index.jsx
+++ src/componentes/navegacao/index.jsx
@@ -1,21 +1,65 @@
+import { useState } from 'react';
+import { Home, User, LogOut } from 'lucide-react';
 export function Navigation() {
+  const [abaAtiva, setAbaAtiva] = useState('inicio');
+
   return (
     <div
       id="navigation"
-      className="  bg-[#0F4418] h-screen w-2xs flex-col flex"
+      className="  bg-[#0F4418] h-screen text-[#E5E7EB] w-80 flex-col flex shadow-[inset_0_20px_20px_-10px_rgba(0,0,0,0.8)]"
     >
-      <h1>Not Fat</h1>
-      <h5>Sua saúde, seu estilo de vida!</h5>
-      <div className="lex-col flex">
+      <h1 className="text-[30px]  mb-1 mt-6 ml-5 " >NOT FAT</h1>
+      <h5  className="text-1xl  mb-4 ml-5">Sua saúde, seu estilo de vida!</h5>
+      <hr className= "border-t border-white/30 mb-3"></hr>
+      <div className="text-[18px] ml-1 mt-4">
         <ul>
           <li>
-            <a href="">Refeições</a>
+            <a href="#" 
+              onClick={(e) => {
+                e.preventDefault();
+                setAbaAtiva('inicio')}}
+              className= {`block mx-3 mb-8 flex items-center gap-3 px-4 py-3 rounded-lg transition-colors ${
+                abaAtiva === 'inicio' 
+                ? 'bg-white/10 text-white' 
+                : 'text-gray-300 hover:bg-white/10 hover:text-white'
+              }`}>
+              <Home className="w-5 h-5" /> Início
+              </a>
+
           </li>
           <li>
-            <a href="">Perfil</a>
+            <a href="#" 
+            onClick={(e) => {
+              e.preventDefault();
+              setAbaAtiva('perfil')}}
+            className= {`block mx-3 mb-8 flex items-center gap-3 px-4 py-3 rounded-lg transition-colors ${
+              abaAtiva === 'perfil' 
+              ? 'bg-white/10 text-white' 
+              : 'text-gray-300 hover:bg-white/10 hover:text-white'
+            }`}>
+              <User className="w-5 h-5" /> Perfil</a>
+          </li>
+          <li>
+            <a href="#" 
+            onClick={(e) => {
+              e.preventDefault();
+              setAbaAtiva('sair da conta')}}
+            className= {`block mt-130 mx-3 mb-8 flex items-center gap-3 px-4 py-3 rounded-lg transition-colors ${
+              abaAtiva === 'sair da conta' 
+              ? 'bg-white/10 text-white' 
+              : 'text-gray-300 hover:bg-white/10 hover:text-white'
+            }`}>
+              
+              <LogOut className="w-5 h-5" /> Sair da conta</a>
           </li>
         </ul>
       </div>
     </div>
   );
 }
+
+/**
+ * bg-[#0F4418]: Aplica um background-color com a cor exata em hexadecimal. 
+h-screen: Define a altura (height) para ocupar 100% da tela (100vh).
+flex e flex-col: Ativam o Flexbox e colocam os itens em coluna (um embaixo do outro).
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Login from "./pages/login";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
+    <Home/>
   </StrictMode>,
 );
